f7916e05e08af48e9ff20ab5660671bf4eb33481
tastypavlova <killickrhys@gmail.com>
2019-08-20 17:09:14 +1000

    Upgrades and Textures
    Auto cookies can now be generated, textures added for the buttons

---
+++ Assets/Scripts/AutoCookie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCookie : MonoBehaviour
+{
+    public bool creatingCookie = false; 
+    public static int cookieIncrease = 1;
+    public int internalIncrease;
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalIncrease = cookieIncrease;
+
+        if (creatingCookie == false)
+        {
+            creatingCookie = true;
+            StartCoroutine(CreateTheCookie());
+        }
+    }
+
+    IEnumerator CreateTheCookie()
+    {
+        GlobalCookies.cookieCount += internalIncrease;
+        yield return new WaitForSeconds(1);
+        creatingCookie = false;
+    }
+}
---
+++ Assets/Scripts/PurchaseLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseLog : MonoBehaviour
+{
+    public GameObject autoCookie;
+
+    public void StartAutoCookie()
+    {
+        autoCookie.SetActive(true);
+    }
+}
