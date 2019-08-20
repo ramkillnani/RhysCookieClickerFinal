a9e50b88e9b3f3308c6baf355c4470f21b1bef42
tastypavlova <killickrhys@gmail.com>
2019-08-20 14:55:15 +1000

    Animations
    Added text animations, made status box display text when there are no more cookies to sell

--- Assets/Scripts/SellCookie.cs
+++ Assets/Scripts/SellCookie.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SellCookie : MonoBehaviour
 {
     public GameObject textBox;
+    public GameObject statusBox;
 
     public void ClickTheButton()
     {
-        GlobalCookies.cookieCount -= 1;
-        GlobalCash.cashCount += 1;
+        if (GlobalCookies.cookieCount == 0)
+        {
+            statusBox.GetComponent<Text>().text = "Not enough Cookies to sell";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalCookies.cookieCount -= 1;
+            GlobalCash.cashCount += 1;
+        }
     }
 }
